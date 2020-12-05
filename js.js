dfcf5cb054cd06a34bd6596332be471ebbe7f574
Zaed Tonmoy <zaedtonmoy@gmail.com>
2020-12-05 13:09:52 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    c=a+b;
+    return c;
+}
+document.write(sum(3,5));
+
+function counter(a){
+    var x=a.length;
+    document.write(x);
+}
+counter("zaedbintonmoy");
+
+for(i=0; i<99; i++){
+    document.write(i +"</br>");
+}
